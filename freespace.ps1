872b205c67f406c068177b373b33032577eb92ac
Papa-Yaw Oppong-Frempong <papa-yaw.oppong-frempong@ibm.com>
2018-08-07 10:39:18 +0100

    reminder of how to commit

---
+++ freespace.ps1
@@ -0,0 +1,11 @@
+﻿
+
+param(
+
+    $unit = '1gb'
+)
+$unitName = "Free space in " + $unit.Substring(1,2)
+
+Get-CimInstance  -class Win32_LogicalDisk | select deviceid, FreeSpace, @{n= $unitName;e={$_.FreeSpace/$unit -as [int]}}
+
+
